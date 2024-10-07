8a4de435e95a8e5645df83745d3346f9ff967397
nicolly1244 <160141600+nicolly1244@users.noreply.github.com>
2024-10-07 12:15:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Grande Porte',
+    'Quais são os nomes dos 4 estômagos de um ruminante?',
+    'Rúmen, Retículo, Omaso e Abomaso'
+)
+
+criaCartao(
+    'Raças',
+    'Qual é a maior raça de gato?',
+    'Maine coon'
+)
+
+criaCartao(
+    'Grande Porte',
+    'Qual animal tem glândulas sudoríparas pelo corpo todo?',
+    'O cavalo'
+)
+
+criaCartao(
+    'Raças',
+    'Qual raça de cachorro tem mais tendência a ter alopecia?',
+    'Yorkshire Terrier'
+)
+
+criaCartao(
+    'Raças',
+    'Qual raça de cachorro tem mais tendência a ter alopecia?',
+    'Yorkshire Terrier'
+)
+
